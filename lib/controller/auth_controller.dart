dbbcb29a0a12dc1f814d483b37e5c352a11da41b
Hamilka <jerome.hamilka@gmail.com>
2023-05-05 01:27:00 +0200

    feature custom widget

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:socialnetwork/util/images.dart';
 
+import '../custom_widget/my_gradient.dart';
+import '../custom_widget/my_textField.dart';
+import '../custom_widget/padding_with.dart';
 import '../model/my_painter.dart';
 
 class AuthController extends StatefulWidget {
@@ -37,13 +40,24 @@ class _AuthControllerState extends State<AuthController> {
     super.dispose();
   }
 
+  hideKeyboard() {
+    FocusScope.of(context).requestFocus(FocusNode());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: InkWell(
-          onTap: () {},
+          onTap: () {
+            hideKeyboard();
+          },
           child: Container(
+            //decoration: MyGradient(startColor: ColorTheme().pointer(), endColor: ColorTheme().base(), horizontal: false),
+            decoration: MyGradient(
+                startColor: Colors.white,
+                endColor: Colors.red,
+                horizontal: false),
             width: MediaQuery.of(context).size.width,
             height: (MediaQuery.of(context).size.height > 700)
                 ? MediaQuery.of(context).size.height
@@ -60,11 +74,9 @@ class _AuthControllerState extends State<AuthController> {
                     flex: 2,
                     child: PageView(
                       controller: _pageController,
-                      children: const [
-                        Text("Se connecter"),
-                        Text("Creer un compte"),
-                        // logCard(true),
-                        // logCard(false),
+                      children: [
+                        logCard(true),
+                        logCard(false),
                       ],
                     ),
                   ),
@@ -109,6 +121,42 @@ class _AuthControllerState extends State<AuthController> {
               duration: const Duration(milliseconds: 500),
               curve: Curves.easeIn);
         },
+      ),
+    );
+  }
+
+  Widget logCard(bool userExists) {
+    List<Widget> list = [];
+    if (!userExists) {
+      list.add(MyTextField(
+        controller: _surname,
+        hint: "Entrez votre prénom",
+      ));
+      list.add(MyTextField(
+        controller: _name,
+        hint: "Entrez votre nom",
+      ));
+    }
+    list.add(MyTextField(
+      controller: _mail,
+      hint: "Adresse mail",
+    ));
+    list.add(MyTextField(
+      controller: _password,
+      hint: "Mot de passe",
+      obscure: true,
+    ));
+
+    return PaddingWith(
+      child: Center(
+        child: Card(
+          elevation: 7.5,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.min,
+            children: list,
+          ),
+        ),
       ),
     );
   }
